4f266ce47f7d305b8af730372cac0fa3b014cbc1
Arczeus <slotalarek@gmail.com>
2025-12-30 01:45:00 +0100

    Initial commit

---
+++ get_colors/create_color_json.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+input_file = "colors.xlsx"
+output_file = "colors.json"
+
+data = pd.read_excel(input_file, dtype={'code': str})
+
+data['name'] = data['name'].str.strip()
+data['name'] = data['name'].str.replace(r'\s*\(\d+\)$', '', regex=True)
+
+data.to_json(output_file, orient='records', indent=4)
